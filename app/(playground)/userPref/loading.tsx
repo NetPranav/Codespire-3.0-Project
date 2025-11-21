aeb44938d96c16d2d31093b8fa2c492ae46a2c8d
NetPranav <162047134+NetPranav@users.noreply.github.com>
2025-11-22 00:29:35 +0530

    Deploy ready

--- app/(playground)/userPref/loading.tsx
+++ app/(playground)/userPref/loading.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div className={`h-screen w-screen fixed top-0 blur(10px) bg-[red] z-99`}>
-      
+    <div className={`h-screen w-screen fixed backdrop-blur-sm z-99 top-0`}>
+        <div id={`loader`}></div>
     </div>
   )
 }
