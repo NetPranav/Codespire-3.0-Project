6c81db1364f556331043acd4888f337acbb75355
NetPranav <162047134+NetPranav@users.noreply.github.com>
2025-12-03 14:36:00 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,33 @@
 import Navbar from "@/components/navbar";
-import Hero from '@/components/hero';
-import Carousal from '@/components/carousal';
-import Footer from '@/components/footer';
+import Hero from "@/components/hero";
+import Carousal from "@/components/carousal";
+import Footer from "@/components/footer";
 import AnimatedSections from "@/components/features";
+import HowItWorks from "@/components/howItworks";
+import FAQ from '@/components/faq'
 
-
-export default function Home() {  
-
+export default function Home() {
   return (
     <div className="h-full w-full bg-[black]">
       <div className="h-screen w-screen fixed bg-[black] z-[-99]"></div>
       <div id="heroBg" className="h-full w-full z-3 absolute inset-0">
-        <video className="w-full h-full object-cover" src='/Hero-Video.mp4' autoPlay loop muted></video>
+        <video
+          className="w-full h-full object-cover"
+          src="/Hero-Video.mp4"
+          autoPlay
+          loop
+          muted
+        ></video>
       </div>
-      <Navbar/>
-      <Hero/>
-      <Carousal/>
+      <Navbar />
+      <Hero />
+      <Carousal />
+      <HowItWorks />
+      <FAQ/>
       {/* <div className="">
         <AnimatedSections/>
       </div> */}
-      <Footer/>
+      <Footer />
     </div>
   );
 }
